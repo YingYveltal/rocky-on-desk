897477f1c73db332e9022fa40d5ffbafe9a532db
yingyaogu <yingyaogu@users.noreply.github.com>
2026-05-06 19:23:34 +0800

    fix: remove --in-process-gpu flag to fix transparent window interaction

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # Launch Rocky on Desk with WSL Electron dependencies
 export LD_LIBRARY_PATH="$HOME/electron-libs/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
 cd "$(dirname "$0")"
-npm start -- --disable-gpu --in-process-gpu --no-sandbox
+npm start -- --disable-gpu --no-sandbox
